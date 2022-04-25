e14b9412a75751ba51a83c875e3e3bc5914bc1e0
Eddy Kim <lasir3@gmail.com>
2022-04-25 10:32:22 +0900

    Update 02SELECT.sql

--- WebContent/WEB-INF/sql/02SELECT.sql
+++ WebContent/WEB-INF/sql/02SELECT.sql
@@ -2,9 +2,12 @@ USE w3schools;
 SELECT * FROM Customers;
 SELECT Country FROM Customers;
 
--- DISTINCT 중복 제거
-SELECT DISTINCTCountry) FROM Customers;
+-- DISTINCT : 중복 제거 
+SELECT DISTINCT(Country) FROM Customers;
 SELECT DISTINCT Country FROM Customers;
 
 -- Suppliers 테이블에서 중복없이 Country 컬럼 조회
 SELECT DISTINCT Country FROM Suppliers;
+SELECT Country FROM Suppliers;
+
+SELECT city FROM Customers;
