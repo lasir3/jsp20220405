70a324d4a4d5933b4c7565a38ab4fcdfa0f654a1
Eddy Kim <lasir3@gmail.com>
2022-04-29 01:47:51 +0900

    Update 17IN.sql

--- WebContent/WEB-INF/sql/17IN.sql
+++ WebContent/WEB-INF/sql/17IN.sql
@@ -11,3 +11,11 @@ WHERE City IN ('Berlin', 'London');
 SELECT City FROM Suppliers;
 SELECT * FROM Customers
 WHERE City IN (SELECT City FROM Suppliers);
+
+SELECT * FROM Customers
+WHERE City NOT IN ('Berlin', 'London');
+
+SELECT City FROM Suppliers;
+
+SELECT * FROM Customers
+WHERE City NOT IN (SELECT City FROM Suppliers);
